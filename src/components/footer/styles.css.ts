f22d82096a12e3b850f43e65852760860a3c0bc1
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-06-06 10:56:14 +0900

    update: add footer component

---
+++ src/components/footer/styles.css.ts
@@ -0,0 +1,17 @@
+import { style } from "@vanilla-extract/css";
+
+export const footer = style({
+  width: "100%",
+  height: "30px",
+  padding: "auto",
+  textAlign: "center",
+  alignContent: "center",
+  backgroundColor: "#282c34",
+  color: "#fff",
+  position: "relative",
+  bottom: 0,
+});
+
+export const footerText = style({
+  margin: "auto",
+});
