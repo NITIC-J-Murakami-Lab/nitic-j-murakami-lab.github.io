e3d73df733cf07c1dd016955e5ce3561e066599d
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-06-06 20:51:23 +0900

    update: fixed config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
-import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
-
-const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {};
 
-export default withVanillaExtract(nextConfig);
+export default nextConfig;
