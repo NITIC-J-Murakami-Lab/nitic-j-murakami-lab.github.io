abcb747e132e08d3be060460020aeb33a67f4e95
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-06-09 18:16:47 +0900

    update: fixed wrap point

--- src/app/access/page.tsx
+++ src/app/access/page.tsx
@@ -20,7 +20,11 @@ const Home: React.FC = () => {
         <h1>Access</h1>
         <section className={commonStyles.section}>
           <h2>Map</h2>
-          <p>〒021-0902 岩手県一関市萩荘字高梨 一関工業高等専門学校 4号棟</p>
+          <p className={styles.address}>
+            <span>〒021-0902</span>
+            <span>岩手県一関市萩荘字高梨</span>
+            <span>一関工業高等専門学校 4号棟</span>
+          </p>
           <iframe
             className={styles.map}
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d775.9980792616687!2d141.10686696969037!3d38.9241530982324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f88d09d6f0b0357%3A0x7ecc4e362c08f769!2z5LiA6Zai5bel5qWt6auY562J5bCC6ZaA5a2m5qCh!5e0!3m2!1sja!2sjp!4v1749460177041!5m2!1sja!2sjp"
